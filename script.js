7d2c8c9c34aac33d0d38320a0c53e0de0429605d
snork-maiden <pollykoldysheva@gmail.com>
2021-10-05 20:31:01 +0300

    Add preloader and error message

--- script.js
+++ script.js
@@ -1,23 +1,53 @@
-'use strict'
+"use strict";
 
-const companiesList = document.querySelector('.companies-list');
+const companiesList = document.querySelector(".companies-list");
+const preloader = document.querySelector(".loading-ring");
 
-fetch('https://jsonplaceholder.typicode.com/users')
-        .then((blob) => blob.json())
-        .then((users) => {  
-  for (let user of users) {
-    createCompanyItem(user);
-}
-const contactPersonButtons = companiesList.querySelectorAll('.company__contact-person')
-contactPersonButtons.forEach( (button) => button.addEventListener('click', () => {
-    button.classList.toggle('company__contact-person--open');
-}))
-});
+preloader.classList.add("loading-ring--active");
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((blob) => blob.json())
+  .then((users) => {
+    preloader.classList.remove("loading-ring--active");
+    for (let user of users) {
+      createCompanyItem(user);
+    }
+
+    const contactPersonButtons = companiesList.querySelectorAll(
+      ".company__contact-person"
+    );
+    contactPersonButtons.forEach((button) =>
+      button.addEventListener("click", () => {
+        button.classList.toggle("company__contact-person--open");
+      })
+    );
+  })
+  .catch(() => {
+    preloader.classList.remove("loading-ring--active");
+    const errorMessage = document.querySelector('.fetch-error');
+    errorMessage.classList.add('fetch-error--active');
+  });
+
+/**
+ * Add to companiesList <li> with info about one company
+ * @param {Object} user
+ * @param {Object} user.company
+ * @param {string} user.company.name
+ * @param {string} user.company.catchPhrase
+ * @param {Object} user.address
+ * @param {string} user.address.city
+ * @param {string} user.address.street
+ * @param {string} user.address.suite
+ * @param {string} user.name
+ * @param {string} user.username
+ * @param {string} user.email
+ * @param {string} user.phone
+ * @param {string} user.website
+ */
 
 function createCompanyItem(user) {
-    const companyItem = document.createElement('li');
-    companyItem.classList.add('companies-list__item');
-    companyItem.innerHTML = `
+  const companyItem = document.createElement("li");
+  companyItem.classList.add("companies-list__item");
+  companyItem.innerHTML = `
     <article class="company">
             <h2 class="company__name">${user.company.name}</h2>
             <p class="company__catch-phrase">
@@ -45,20 +75,25 @@ function createCompanyItem(user) {
                   >${user.website}</a
                 >
               </li>
-              <li class="contacts__item">${user.address.city}, ${user.address.street}, ${user.address.suite}</li>
+              <li class="contacts__item">${user.address.city}, ${
+    user.address.street
+  }, ${user.address.suite}</li>
             </ul>
           </div>
         </article>
-    `
-    companiesList.appendChild(companyItem);
-
+    `;
+  companiesList.appendChild(companyItem);
 }
 
-function formatPhone (number) {
-    number = number.split(' ')[0];
-    return '+' + number.replace(/[^0-9]/g, '');
+/**
+ * format phone for tel href
+ * @param {string} phoneNumber
+ * @returns {string}
+ */
+function formatPhone(phoneNumber) {
+  phoneNumber = phoneNumber.split(" ")[0];
+  return "+" + phoneNumber.replace(/[^0-9]/g, "");
 }
-
 
 // {
 //     "id": 1,
