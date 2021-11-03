ddf8a0be6518d4dfc6a96099acd0e9eb4e129a86
snork-maiden <pollykoldysheva@gmail.com>
2021-11-03 18:30:31 +0300

    Add search in companies list

--- script.js
+++ script.js
@@ -114,26 +114,21 @@ function formatPhone(phoneNumber) {
   return "+" + phoneNumber.replace(/[^0-9]/g, "");
 }
 
-// {
-//     "id": 1,
-//     "name": "Leanne Graham",
-//     "username": "Bret",
-//     "email": "Sincere@april.biz",
-//     "address": {
-//       "street": "Kulas Light",
-//       "suite": "Apt. 556",
-//       "city": "Gwenborough",
-//       "zipcode": "92998-3874",
-//       "geo": {
-//         "lat": "-37.3159",
-//         "lng": "81.1496"
-//       }
-//     },
-//     "phone": "1-770-736-8031 x56442",
-//     "website": "hildegard.org",
-//     "company": {
-//       "name": "Romaguera-Crona",
-//       "catchPhrase": "Multi-layered client-server neural-net",
-//       "bs": "harness real-time e-markets"
-//     }
-//   }
+function displayMatches() {
+  let companies = companiesList.children;
+  const query = this.value.toLowerCase();
+
+  for (let company of companies) {
+    company.hidden = false;
+
+    const text = company.textContent.toLowerCase();
+    if(!text.includes(query)) {
+      company.hidden = true;
+    }
+  }
+
+}
+
+const searchInput = document.querySelector(".search__input");
+searchInput.addEventListener("change", displayMatches);
+searchInput.addEventListener("keyup", displayMatches);
